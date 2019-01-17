1bbb3d224f79ea4778953ddeb0549d25ad5da907
Ben Bowmer <benjamin.bowmer@gmail.com>
2019-01-17 20:46:29 +1030

    OLED

--- main/RGEO.c
+++ main/RGEO.c
@@ -388,82 +388,6 @@ static esp_err_t i2c_write(i2c_port_t i2c_num, uint8_t* data_wr, size_t size)
 	return ret;
 }
 
-//void Adafruit_SSD1306::ssd1306_command1(uint8_t c) {
-//	if (wire) {
-//		 // I2C
-//	  wire->beginTransmission(i2caddr);
-//		WIRE_WRITE((uint8_t)0x00);  // Co = 0, D/C = 0
-//		WIRE_WRITE(c);
-//		wire->endTransmission();
-//	}
-//	else {
-//		 // SPI (hw or soft) -- transaction started in calling function
-//	  SSD1306_MODE_COMMAND
-//	  SPIwrite(c);
-//	}
-//}
-//
-//// Issue list of commands to SSD1306, same rules as above re: transactions.
-//// This is a private function, not exposed.
-//void Adafruit_SSD1306::ssd1306_commandList(const uint8_t *c, uint8_t n) {
-//	if (wire) {
-//		 // I2C
-//	  wire->beginTransmission(i2caddr);
-//		WIRE_WRITE((uint8_t)0x00);  // Co = 0, D/C = 0
-//		uint8_t bytesOut = 1;
-//		while (n--) {
-//			if (bytesOut >= WIRE_MAX) {
-//				wire->endTransmission();
-//				wire->beginTransmission(i2caddr);
-//				WIRE_WRITE((uint8_t)0x00);  // Co = 0, D/C = 0
-//				bytesOut = 1;
-//			}
-//			WIRE_WRITE(pgm_read_byte(c++));
-//			bytesOut++;
-//		}
-//		wire->endTransmission();
-//	}
-//	else {
-//		 // SPI -- transaction started in calling function
-//	  SSD1306_MODE_COMMAND
-//	  while(n--) SPIwrite(pgm_read_byte(c++));
-//	}
-//}
-
-
-//https://github.com/ExploreEmbedded/8051_DevelopmentBoard/blob/master/Code/Keil_Sample_Codes/00-libfiles/oled_i2c.c
-//https://exploreembedded.com/wiki/OLED_Interface_With_8051
-//void OLED_Init(void)
-//{ 
-//	oledSendCommand(SSD1306_DISPLAY_OFF);
-//	oledSendCommand(SSD1306_SET_DISPLAY_CLOCK_DIV_RATIO);
-//	oledSendCommand(0x80);
-
-//	oledSendCommand(SSD1306_SET_MULTIPLEX_RATIO);
-//	oledSendCommand(0x3F);
-//	oledSendCommand(SSD1306_SET_DISPLAY_OFFSET);
-//	oledSendCommand(0x0);
-//	oledSendCommand(SSD1306_SET_START_LINE | 0x0);
-//	oledSendCommand(SSD1306_CHARGE_PUMP);
-//	oledSendCommand(0x14);
-//	oledSendCommand(SSD1306_MEMORY_ADDR_MODE);
-//	oledSendCommand(0x00);
-//	oledSendCommand(SSD1306_SET_SEGMENT_REMAP | 0x1);
-//	oledSendCommand(SSD1306_COM_SCAN_DIR_DEC);
-//	oledSendCommand(SSD1306_SET_COM_PINS);
-//	oledSendCommand(0x12);
-//	oledSendCommand(SSD1306_SET_CONTRAST_CONTROL);
-//	oledSendCommand(0xCF);
-//	oledSendCommand(SSD1306_SET_PRECHARGE_PERIOD);
-//	oledSendCommand(0xF1);
-//	oledSendCommand(SSD1306_SET_VCOM_DESELECT);
-//	oledSendCommand(0x40);
-//	oledSendCommand(SSD1306_DISPLAY_ALL_ON_RESUME);
-//	oledSendCommand(SSD1306_NORMAL_DISPLAY);
-//	oledSendCommand(SSD1306_DISPLAY_ON);
-//
-//	OLED_Clear(); /* Clear the complete LCD during init */
-//}
 
 
 
@@ -477,10 +401,10 @@ static void LCDTask()
 	conf.mode = I2C_MODE_MASTER;
 	conf.sda_io_num = GPIO_NUM_21;
 	//	conf.sda_io_num = GPIO_NUM_18;
-		conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
 	conf.scl_io_num = GPIO_NUM_22;
 	//	conf.scl_io_num = GPIO_NUM_19;
-		conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
 	conf.master.clk_speed = 100000;
 	i2c_param_config(i2c_master_port, &conf);
 	i2c_driver_install(i2c_master_port,
@@ -490,81 +414,119 @@ static void LCDTask()
 		0);
 	
 	
-	volatile uint8_t *dat = malloc(1030);
+	uint16_t i2cIndex;
+	volatile uint8_t *dat = malloc(1000);
 	esp_err_t ret;
 
-	memset(dat, 0, 1030);
+	memset(dat, 0, 1000);
 
 	ESP_LOGI(LCD, "Init LCD");
 
 	
 	#define HEIGHT 32
 	
-	*(dat +  0) = SSD1306_DISPLAYOFF;  			// 0xAE
-	*(dat +  1) = SSD1306_SETDISPLAYCLOCKDIV;  	// 0xD5
-	*(dat +  2) = 0x80;  						// the suggested ratio 0x80
-	*(dat +  3) = SSD1306_SETMULTIPLEX;          // 0xA8
-	*(dat +  4) = HEIGHT - 1;
-	*(dat +  5) = SSD1306_SETDISPLAYOFFSET;  	// 0xD3
-	*(dat +  6) = 0x0;  							// no offset
-	*(dat +  7) = SSD1306_SETSTARTLINE | 0x0;  	// line #0
-	*(dat +  8) = SSD1306_CHARGEPUMP;  			// 0x8D
-	*(dat +  9) = 0x10;  						// Could be 0x10, could be 0x14
-	*(dat + 10) = SSD1306_MEMORYMODE;  			// 0x20
-	*(dat + 11) = 0x00;  						// 0x0 act like ks0108
-	*(dat + 12) = SSD1306_SEGREMAP | 0x1;
-	*(dat + 13) = SSD1306_COMSCANDEC;
-	*(dat + 14) = SSD1306_SETCOMPINS;  			// 0xDA
-	*(dat + 15) = 0x02;
-	*(dat + 16) = SSD1306_SETCONTRAST;  			// 0x81
-	*(dat + 17) = 0x8F;
-	*(dat + 18) = SSD1306_SETPRECHARGE;  		// 0xd9
-	*(dat + 19) = 0x22;  						// Could be 0x22, could be 0xF1
-	*(dat + 20) = SSD1306_SETVCOMDETECT;  		// 0xDB
-	*(dat + 21) = 0x40;
-	*(dat + 22) = SSD1306_DISPLAYALLON_RESUME;  	// 0xA4
-	*(dat + 23) = SSD1306_NORMALDISPLAY;  		// 0xA6
-	*(dat + 24) = SSD1306_DEACTIVATE_SCROLL;
-	*(dat + 25) = SSD1306_DISPLAYON;
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;     							// 0x00
+	*(dat + i2cIndex++) = SSD1306_DISPLAYOFF;     			// 0xAE
+	*(dat + i2cIndex++) = SSD1306_SETDISPLAYCLOCKDIV;    	// 0xD5
+	*(dat + i2cIndex++) = 0x80;    						// the suggested ratio 0x80
+	*(dat + i2cIndex++) = SSD1306_SETMULTIPLEX;            // 0xA8
 
-	ret = i2c_write(I2C_NUM_0, dat, 26);
-	
-	
-	
-	// fill buffer with something for test
-	memset(display_buffer, 0X02, 1024);   // tried other values
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
 
-	
-	uint16_t j = 0;
- 
-	
-	
-	*(dat +  0) = SSD1306_COLUMNADDR;			// 0x21 COMMAND
-	*(dat +  1) = 0;							// Column start address
-	*(dat +  2) = SSD1306_LCDWIDTH - 1;			// Column end address
-	*(dat +  3) = 0; 							
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;      							// 0x00
+	*(dat + i2cIndex++) = HEIGHT - 1;
 
-	ret = i2c_write(I2C_NUM_0, dat, 3);
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
 
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = SSD1306_SETDISPLAYOFFSET;     	// 0xD3
+	*(dat + i2cIndex++) = 0x0;    							// no offset
+	*(dat + i2cIndex++) = SSD1306_SETSTARTLINE | 0x0;    	// line #0 0x40
+	*(dat + i2cIndex++) = SSD1306_CHARGEPUMP;    			// 0x8D
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = 0x14;     						// Could be 0x10, could be 0x14
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = SSD1306_MEMORYMODE;      			// 0x20
+	*(dat + i2cIndex++) = 0x00;    						// 0x0 act like ks0108
+	*(dat + i2cIndex++) = SSD1306_SEGREMAP | 0x1;
+	*(dat + i2cIndex++) = SSD1306_COMSCANDEC;
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = SSD1306_SETCOMPINS;     			// 0xDA
+	*(dat + i2cIndex++) = 0x02;
+	*(dat + i2cIndex++) = SSD1306_SETCONTRAST;    			// 0x81
+	*(dat + i2cIndex++) = 0x8F;
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = SSD1306_SETPRECHARGE;     		// 0xd9
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = 0xF1;     						// Could be 0x22, could be 0xF1
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = SSD1306_SETVCOMDETECT;     		// 0xDB
+	*(dat + i2cIndex++) = 0x40;
+	*(dat + i2cIndex++) = SSD1306_DISPLAYALLON_RESUME;    	// 0xA4
+	*(dat + i2cIndex++) = SSD1306_NORMALDISPLAY;    		// 0xA6
+	*(dat + i2cIndex++) = SSD1306_DEACTIVATE_SCROLL;
+	*(dat + i2cIndex++) = SSD1306_DISPLAYON;
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
 	
-	*(dat +  0) = SSD1306_PAGEADDR;				// 0x22 COMMAND
-	*(dat +  1) = 0;							// Start Page address
-	*(dat +  2) = (SSD1306_LCDHEIGHT / 8) - 1;	// End Page address
-	*(dat +  3) = 0; 							
+	
+	
+	
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0x00;  							// 
+	*(dat + i2cIndex++) = 0x22;   				// 0x22 COMMAND
+	*(dat + i2cIndex++) = 0x00;  							// Start Page address
+	*(dat + i2cIndex++) = 0xFF;   							// Start Page address
+	*(dat + i2cIndex++) = 0x21; 							
+	*(dat + i2cIndex++) = 0x00;
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+
+	i2cIndex = 0;
+	*(dat + i2cIndex++) = 0;       							// 0x00
+	*(dat + i2cIndex++) = 0x7F; 							
+
+	ret = i2c_write(I2C_NUM_0, dat, i2cIndex);
+	
+	
+	memset(display_buffer, 0XA5, 33);   
+
+	display_buffer[0] = 0x40;
 	
 
-
-	ret = i2c_write(I2C_NUM_0, dat, 3);
-		
-//	*(dat +  0) = 0x40;
-	
-	ret = i2c_write(I2C_NUM_0, (void *)0x40, 1);
-
-	for (uint16_t j = 0; j < 1024; j++)
+	for (uint16_t j = 0; j < 16; j++)
 	{
-		i2c_write(I2C_NUM_0, (display_buffer + j), 1);
+		i2c_write(I2C_NUM_0, display_buffer, 32);
 	}
-	
+
+	i2c_write(I2C_NUM_0, display_buffer, 17);
+
 	
 	ESP_LOGW(LCD, "Well that worked at least");
 	
